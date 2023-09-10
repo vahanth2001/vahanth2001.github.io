d2943f1c75f8cbbbcf5481aee17f67ae66a12521
vahanth2001 <vahanthguna765992@gmail.com>
2023-09-10 20:23:30 +0530

    f

--- src/components/Main/Home/Home.jsx
+++ src/components/Main/Home/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Typewriter from 'typewriter-effect'
 
 function Home() {
+  const words =<Typewriter options={{strings: ["Designer", "Developer", "Freelancer"],autoStart: true,loop: true,}}/>
   return (
-    <div>Home</div>
+    <div class="hero-container" data-aos="fade-in">
+      <h1>Boinpally Vahanth Phalguna</h1>
+      <h5>I'm a {words}</h5>
+    </div>
   )
 }
 
